b4f3cbdcac24f9d12f98cf9bbfc469a05554fbb5
eagleapex <eagleapex@gmail.com>
2013-11-13 23:01:52 -0500

    added a Pin variable and lots of comments

--- plugins/RedstoneLamp.py
+++ plugins/RedstoneLamp.py
@@ -1,3 +1,13 @@
+# Redstone Lamp project developed by Chris Thompson
+# Redstone Lamp plugin by Kyle Yankanich
+# 
+# This plugin listens for a specific /tell message containing the word "on" or "off"
+# This could be any string
+# "On" will turn the pin high
+# "Off" will turn the pin low
+# For the Redstone Lamp replica, pin 16 is connected to the base on a NPN transistor to control 12V LEDs
+# CC-BY-SA 2013
+
 import string
 import copy
 import base64
@@ -5,11 +15,13 @@ import re
 from datetime import datetime
 import RPi.GPIO as GPIO
 
+pin = 16 #the GPIO on the RPi.
+
 class RedstoneLamp:
 
     def onEnable(self, parser, pluginloader):
-	GPIO.setup(16, GPIO.OUT)
-	GPIO.output(16, False)
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, False)
 
 
     def packetReceive(self, packetID, receivedPacket):
@@ -19,12 +31,12 @@ class RedstoneLamp:
 		
 		if re.search(r'on', packet['Message']):
 			print "REDSTONE LAMP ON MOFO\n";
-			GPIO.output(16, True)
+			GPIO.output(pin, True)
 			with open("redstone.log", "a") as myfile:
 				myfile.write(str(datetime.now()) + " - Redstone on\n")
 
 		if re.search(r'off', packet['Message']):
 			print "REDSTONE LAMP OFF MOFO\n";
-			GPIO.output(16, False)
+			GPIO.output(pin, False)
 			with open("redstone.log", "a") as myfile:
 				myfile.write(str(datetime.now()) + " - Redstone off\n")
